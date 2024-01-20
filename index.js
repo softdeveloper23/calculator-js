8bd3d67fbad49bf987d9d49d0f7ee33bed1be72b
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-20 15:33:55 +0100

    Finished the application

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Calculator Program
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value); // Warning: eval() is dangerous
+    }
+    catch {
+        display.value = 'Error';
+    }
+}
